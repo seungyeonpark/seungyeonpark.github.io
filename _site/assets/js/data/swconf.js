ffba640d52e66c39e7dabfdcf3a65f452902b8eb
sypark <siaayeon@gmail.com>
2025-01-25 19:46:40 +0900

    Update config

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737800645',resources: [
+    cacheName: 'chirpy-1737801853',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
