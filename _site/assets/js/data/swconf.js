8f75205f307d8a31be143f0765845e6c852ed206
sypark <siaayeon@gmail.com>
2025-01-30 23:40:20 +0900

    Update post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737869373',resources: [
+    cacheName: 'chirpy-1738247850',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
